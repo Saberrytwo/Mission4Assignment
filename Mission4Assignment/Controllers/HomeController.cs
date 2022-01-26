efa176e19ebcbac17292806609515c844ddc9ee0
Saberrytwo <90337853+Saberrytwo@users.noreply.github.com>
2022-01-26 14:35:06 -0700

    Add project files.

---
+++ Mission4Assignment/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        //Underscore to indicate that it's a private variable
+        private MovieEntryContext _blahContext { get; set; }
+
+        //Constructor
+        public HomeController(ILogger<HomeController> logger, MovieEntryContext randomName)
+        {
+            _logger = logger;
+            _blahContext = randomName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieEntry()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieEntry(MovieEntry en)
+        {
+            _blahContext.Add(en);
+            _blahContext.SaveChanges();
+            return View("EntryCon", en);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission4Assignment/Models/MovieEntryContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4Assignment.Models
+{
+    public class MovieEntryContext :DbContext
+
+    {
+        public MovieEntryContext (DbContextOptions<MovieEntryContext> options) : base(options)
+        {
+            //leave blank for now
+        }
+
+        public DbSet<MovieEntry> Responses { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder mb)  //as long as its in the chain/inherited its ok
+        {
+            mb.Entity<MovieEntry>().HasData(
+                new MovieEntry
+                {
+                    MovieId = 1,
+                    Category = "Animated",
+                    Title = "How to Train Your Dragon",
+                    Year = 2010,
+                    Director = "IDK",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "Nah",
+                    Notes = "Favorite Movie Ever"
+
+                },
+                new MovieEntry
+                {
+                    MovieId = 2,
+                    Category = "Action",
+                    Title = "Interstellar",
+                    Year = 2014,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "Nah",
+                    Notes = "Top 5"
+
+                },
+                new MovieEntry
+                {
+                    MovieId = 3,
+                    Category = "Soul-Searching",
+                    Title = "Manchester by the Sea",
+                    Year = 2016,
+                    Director = "Kenneth Logernan",
+                    Rating = "R",
+                    Edited = false,
+                    LentTo = "Nah",
+                    Notes = "Promising"
+
+                }
+
+                );
+        }
+       
+
+    }
+}
